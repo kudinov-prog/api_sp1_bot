1f3ef3d7fea7a10841fda9c3f715996cc1fe38c6
Kudinov Mikhail <kudinov.mos@gmail.com>
2020-07-16 17:46:30 +0300

    Create log

--- homework.py
+++ homework.py
@@ -1,3 +1,4 @@
+import logging
 import os
 import requests
 import telegram
@@ -9,33 +10,44 @@ load_dotenv()
 
 PRACTICUM_TOKEN = os.getenv("PRACTICUM_TOKEN")
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
-CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
+CHAT_ID = os.getenv('CHAT_ID')
+
+bot = telegram.Bot(token=TELEGRAM_TOKEN)
 
 
 def parse_homework_status(homework):
     homework_name = homework['homework_name']
     status = homework['status']
-    if status == 'rejected':
-        verdict = 'К сожалению в работе нашлись ошибки.'
-    else:
-        verdict = 'Ревьюеру всё понравилось, можно приступать к следующему уроку.'
-    return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
+
+    if homework_name and status:
+        status_list = ('approved', 'rejected')
+        if status in status_list:
+            if status == 'rejected':
+                verdict = 'К сожалению в работе нашлись ошибки.'
+            else:
+                verdict = 'Ревьюеру всё понравилось, можно приступать к следующему уроку.'
+            return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
+    return 'Неверный ответ сервера'
 
 
 def get_homework_statuses(current_timestamp):
+    if current_timestamp is None:
+        current_timestamp = 0
     headers = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
     params = {
         'from_date': current_timestamp
     }
-    homework_statuses = requests.get(
-        'https://praktikum.yandex.ru/api/user_api/homework_statuses/',
-        params=params, headers=headers
-    )
-    return homework_statuses.json()
+    try:
+        homework_statuses = requests.get(
+            'https://praktikum.yandex.ru/api/user_api/homework_statuses/',
+            params=params, headers=headers
+        )
+        return homework_statuses.json()
+    except Exception:
+        logging.debug(f"Запрос не выполнен", exc_info=True)
 
 
 def send_message(message):
-    bot = telegram.Bot(token=TELEGRAM_TOKEN)
     return bot.send_message(chat_id=CHAT_ID, text=message)
 
 
